47076082bac46aed815b43b3e4bde7b23301264c
dhasane <danihas@live.com>
2020-05-09 22:41:53 -0500

    creo que ya esta funcionando

--- flutter_app/lib/MapView.dart
+++ flutter_app/lib/MapView.dart
@@ -2,24 +2,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/listaPaises.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class mapView extends StatelessWidget {
 
   final Pais pais;
+
   mapView({Key key, @required this.pais}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-              pais.getName.toString()
-          ),
-        ),
-//      body: () => {},
+    Size screenSize = MediaQuery.of(context).size;
+    double heigh = screenSize.height;
+    TextStyle whiteStyle = new TextStyle(fontSize: 20.0, color: Colors.white);
+    return new Directionality(
+      textDirection: TextDirection.rtl,
+      child: new Container(
+          padding: new EdgeInsets.only(bottom: 10.0, left: 1.0, right: 1.0),
+          color: Colors.white,
+          child: new FlutterMap(
+            options: new MapOptions(
+                center: new LatLng(pais.getLatlng[0], pais.getLatlng[1]),
+                zoom: 5.0,
+                maxZoom: 15.0,
+                minZoom: 3.0,
+            ),
+            layers: [
+              new TileLayerOptions(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c']),
+//              new MarkerLayerOptions(markers: markers)
+            ],
+          )
+      )
     );
   }
-
 }
+
